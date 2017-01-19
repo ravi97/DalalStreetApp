99ec2e9d26ea4dd5e4fad77b535bd5f9663008d5
Ravikiran <raviramesh.kiran97@gmail.com>
2017-01-19 19:47:15 +0530

    toolbar done for home activity

--- DalalStreet17/app/src/main/java/org/pragyan/dalalstreet17/Home.java
+++ DalalStreet17/app/src/main/java/org/pragyan/dalalstreet17/Home.java
@@ -1,8 +1,13 @@
 package org.pragyan.dalalstreet17;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -11,7 +16,45 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar=(Toolbar)findViewById(R.id.app_bar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater= getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_help:
+                help();
+                return true;
+            case R.id.action_logout:
+                logout();
+                return true;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void help(){
+        Toast.makeText(this, "help clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void logout(){
+        Intent intent=new Intent(this,Login.class);
+        startActivity(intent);
     }
 }
